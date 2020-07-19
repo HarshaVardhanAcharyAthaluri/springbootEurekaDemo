247d0f382623fa116548927139062ecf1aa02208
HarshaVardhan <Harshavardhan.Athaluri@ge.com>
2020-07-19 21:55:45 +0530

    spring cloud

--- orderservice/src/main/java/com/springbootdemo/orderservice/OrderController.java
+++ orderservice/src/main/java/com/springbootdemo/orderservice/OrderController.java
@@ -1,9 +1,13 @@
 package com.springbootdemo.orderservice;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 @RestController
 public class OrderController {
@@ -12,10 +16,19 @@ public class OrderController {
 	private RestTemplate restTemplate;
 	
 	@RequestMapping("/orderhello")
+	@HystrixCommand(fallbackMethod = "getuserfallback",commandKey = "getuser")
 	public String getUser() {
 		String url = "http://USERSERVICE/hello";
 		
 		return restTemplate.getForObject(url, String.class);
 	}
 
+	@GetMapping("/hello")
+	public String helloorder() {
+		return "Hello from OrderService";
+	}
+	
+	public String getuserfallback() {
+	return "Fallback happened for GetUser";
+	}
 }
---
+++ zullproxydemo/src/test/java/com/api/zullproxydemo/ZullproxydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.zullproxydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZullproxydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
